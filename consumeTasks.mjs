109e3c4e6268c4f1e03e834c8a5c3230b8e0223a
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2023-09-12 06:48:00 +1000

    fix: bullmq queue name

--- consumeTasks.mjs
+++ consumeTasks.mjs
@@ -1,21 +1,20 @@
-import { Worker } from 'bullmq';
+import { Worker } from "bullmq";
 import { createArticle } from "./tasks/createArticle.mjs";
 
-const worker = new Worker('createArticle', async job => {
-  if (job.name === 'createArticle') {
+const worker = new Worker("taskQueue", async (job) => {
+  if (job.name === "createArticle") {
     await createArticle(job.data);
   }
 });
 
-worker.on('active', job => {
+worker.on("active", (job) => {
   console.log(`${job.id} has started!`);
-})
+});
 
-worker.on('completed', job => {
+worker.on("completed", (job) => {
   console.log(`${job.id} has completed!`);
 });
 
-worker.on('failed', (job, err) => {
+worker.on("failed", (job, err) => {
   console.log(`${job.id} has failed with ${err.message}`);
 });
-
