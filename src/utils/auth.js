b9d8ca38d0f68fbe67a02660e589f8501b270571
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2023-09-23 10:11:28 +1000

    feat: simple cookie auth

---
+++ src/utils/auth.js
@@ -0,0 +1,30 @@
+if (typeof import.meta.env.APP_PASSWORD === "undefined") {
+  throw new Error(
+    "APP_PASSWORD environment variable is not defined. Please define it."
+  );
+}
+
+const APP_PASSWORD = import.meta.env.APP_PASSWORD;
+
+export function checkLoggedIn(Astro) {
+  const passwordCookie = Astro.cookies.get("astro-sqlite-tts-feed-password");
+  if (!passwordCookie) return false;
+
+  const password = passwordCookie.value;
+
+  return password === APP_PASSWORD;
+}
+
+export function checkPassword(password) {
+  return password === APP_PASSWORD;
+}
+
+export function setPasswordCookie(Astro, password) {
+  Astro.cookies.set("astro-sqlite-tts-feed-password", password, {
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+    path: "/",
+    sameSite: "lax",
+    secure: true,
+    httpOnly: true,
+  });
+}
