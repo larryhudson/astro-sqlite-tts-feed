924fef61dd49f19ea363c5d503cb2f9d63a76105
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2023-09-26 08:07:51 +1000

    feat: deleting articles

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,5 +1,6 @@
 import Database from "better-sqlite3";
 import path from "path";
+import fs from "fs";
 // Having trouble with the path to the DB file.
 // will need to try to work out how to figure out the project root path
 // Eg: https://stackoverflow.com/questions/10265798/determine-project-root-from-a-running-node-js-application
@@ -36,14 +37,14 @@ export function createArticleInDb({
   mp3Length,
 }) {
   const createArticleStatement = db.prepare(
-    "INSERT INTO articles (title, url, mp3Url, mp3Duration, mp3Length) VALUES (?, ?, ?, ?, ?)"
+    "INSERT INTO articles (title, url, mp3Url, mp3Duration, mp3Length) VALUES (?, ?, ?, ?, ?)",
   );
   const result = createArticleStatement.run(
     title,
     url,
     mp3Url,
     mp3Duration,
-    mp3Length
+    mp3Length,
   );
   const createdArticleId = result.lastInsertRowid;
   return createdArticleId;
@@ -65,5 +66,32 @@ export function updateArticleInDb(id, columnsToUpdate) {
     console.log(`Article with ID ${id} updated successfully.`);
   } else {
     console.log(`No article found with ID ${id}.`);
+  }
+}
+
+export function deleteArticleFromDb(id) {
+  const lookupStatement = db.prepare("SELECT * FROM articles WHERE id = ?");
+  const article = lookupStatement.get(id);
+
+  const mp3Url = article.mp3Url;
+  const mp3FilePath = mp3Url.slice(1);
+
+  const mp3PathExists = fs.existsSync(mp3FilePath);
+  if (mp3PathExists) {
+    // delete the file
+    fs.unlinkSync(mp3FilePath);
+    console.log(`Deleted mp3 file at ${mp3FilePath}`);
+  } else {
+    console.log(`No mp3 file found at ${mp3FilePath}`);
+  }
+
+  const deleteStatement = db.prepare("DELETE FROM articles WHERE id = ?");
+  const result = deleteStatement.run(id);
+  if (result.changes > 0) {
+    console.log(`Article with ID ${id} deleted successfully.`);
+    return true;
+  } else {
+    console.log(`No article found with ID ${id}.`);
+    throw new Error(`No article found with ID ${id}.`);
   }
 }
